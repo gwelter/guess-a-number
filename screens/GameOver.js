3e7cde48b3f7c1f6c1445065ed47f921b668f59f
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-02-16 20:08:16 -0300

    Displaying local Image

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import DefaultStyles from "../constants/defaultStyles";
 
 export default function GameOver(props) {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over</Text>
-      <Text>Number of rounds: {props.guessRounds}</Text>
-      <Text>The number was: {props.userNumber}</Text>
+      <Text style={DefaultStyles.bodyText}>The Game is Over</Text>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/success.png")} />
+      </View>
+      <Text style={DefaultStyles.bodyText}>
+        Number of rounds: {props.guessRounds}
+      </Text>
+      <Text style={DefaultStyles.bodyText}>
+        The number was: {props.userNumber}
+      </Text>
       <Button title="NEW GAME" onPress={props.onNewGame} />
     </View>
   );
@@ -17,5 +25,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden"
+  },
+  image: {
+    height: "100%",
+    width: "100$"
   }
 });
